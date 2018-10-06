52d65ecd29d747c3bc75d6d8f01a5c6aabaa9c3e
koneone2002 <katie@imagings-MacBook-Pro.local>
2018-10-06 16:06:14 -0700

    new files

---
+++ build/precache-manifest.7ebb3780f317b8873df0be1fc9fd6888.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "8d5779f05cbf7009c26f",
+    "url": "/clicky-game/static/js/main.8d5779f0.chunk.js"
+  },
+  {
+    "revision": "f55099ee547d85cd671b",
+    "url": "/clicky-game/static/js/1.f55099ee.chunk.js"
+  },
+  {
+    "revision": "8d5779f05cbf7009c26f",
+    "url": "/clicky-game/static/css/main.1c9702fa.chunk.css"
+  },
+  {
+    "revision": "c48d43cf57c2e03ad99afe2df4abb86b",
+    "url": "/clicky-game/index.html"
+  }
+];
