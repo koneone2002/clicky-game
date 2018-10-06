caf79063d16c0cdca881ee7dc0a2d2360ea12bc8
koneone2002 <katie@imagings-MacBook-Pro.local>
2018-10-06 16:35:13 -0700

    another try

---
+++ build/precache-manifest.7811909cce60d9551911ea5a739a04f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "94642e8a06c32b4b3713",
+    "url": "/clicky-game/static/js/main.94642e8a.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clicky-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "94642e8a06c32b4b3713",
+    "url": "/clicky-game/static/css/main.9f77bcad.chunk.css"
+  },
+  {
+    "revision": "a267ed3751f37cfe6e829561aaa52ab3",
+    "url": "/clicky-game/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.7ebb3780f317b8873df0be1fc9fd6888.js"
+  "/clicky-game/precache-manifest.7811909cce60d9551911ea5a739a04f6.js"
 );
 
 workbox.clientsClaim();
